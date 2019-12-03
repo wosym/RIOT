3eee4df8c6fe8318429835277846b875270f0282
Wouter <wsymons@nalys-group.com>
2019-12-03 12:09:47 +0100

    Add print_parsed function + cleanup code

--- examples/doip/main.c
+++ examples/doip/main.c
@@ -14,18 +14,14 @@ int main(void)
 {
     doip_sa source = 0xe801;
     doip_ta target = 0x1fff;
-    uint16_t req_tester_present = 0x8001;           //TODO: implement these as enum in header
-    uint16_t vehicle_present_request = 0x0001;
+    uint8_t uds_data[] = {UDS_SERVICES_TP, UDS_SUPRESS_REPLY};
+
     puts("Starting doip example");
-    //uint8_t testdata[] = {0xaa, 0xab, 0xac};
-    uint8_t uds_data[] = {0x3e, 0x80};
     xtimer_usleep(2000000);     //necesarry for some reason when sending UDP
-    puts("Sending ");
-    //doip_data_request(0, 0, physical, NULL, 0);
-    //doip_data_request(0xabcd, 0xdcba, physical, testdata , 3);
+
     while(1) {
-        doip_send_udp(source, target, req_tester_present, uds_data , 6);
-        doip_send_udp(0, 0, vehicle_present_request, NULL , 0);  //Sending vehicle identification request --> SA and TA ignored
+        doip_send_udp(source, target, DOIP_DIAGNOSTIC_MESSAGE, uds_data , 2);
+        doip_send_udp(0, 0, DOIP_VEHICLE_IDENTIFICATION_REQUEST, NULL , 0);
         xtimer_usleep(1000000);
     }
     return 0;
--- sys/doip/doip.c
+++ sys/doip/doip.c
@@ -7,6 +7,8 @@
 //data buffer for doip message
 uint8_t dbuf[DOIP_MAX_MSG_LEN];
 
+const uint32_t udp_recv_timeout = 1000000;  //us
+
 static void doip_print_msg(uint8_t* data, uint32_t dlen)
 {
     uint32_t i = 0;
@@ -16,6 +18,65 @@ static void doip_print_msg(uint8_t* data, uint32_t dlen)
     }
     printf("\n");
 }
+
+static int doip_print_msg_parsed(uint8_t* data, uint32_t dlen)
+{
+    uint16_t payload_type = 0;
+    uint32_t payload_len = 0;
+    uint16_t source = 0;
+    uint16_t target = 0;
+    uint32_t i = 0;
+    if(dlen < 8 || data == NULL)    //basic DoIP is always 8 bytes. TA and SA can be dropped
+        return -1;
+    if(data[0] == 0x0)
+            puts("DoIp version: 01");
+    if(data[0] == 0x02)
+            puts("DoIP version: 02");
+
+    payload_type = (data[2] << 8 | data[3]);
+    //printf("payload_type: %x\n", payload_type);
+    switch(payload_type) {
+        case DOIP_VEHICLE_IDENTIFICATION_REQUEST:
+            puts("Payload Type: Vehicle Identification Request");
+            break;
+        case DOIP_VEHICLE_IDENTIFCATION_RESPONSE:
+            puts("Payload Type: Vehicle Identification Reply");
+            break;
+        case DOIP_DIAGNOSTIC_MESSAGE:
+            puts("Payload Type: Diagnostic Message Request");
+            break;
+        case DOIP_DIAGNOSTIC_MESSAGE_POSITIVE_ACK:
+            puts("Payload Type: Diagnostic Message Positive Acknowledge");
+            break;
+        //TODO: implement others!
+        default:
+            puts("Unknown payload type");
+            break;
+    }
+    payload_len = (data[4] << 24 | data[5] << 16 | data[6] <<8 | data[7]);
+    printf("Payload length: %ld\n", payload_len);
+
+    if(dlen < 12)
+        return 0;
+    source = (data[8] << 8 | data[9]);
+    target = (data[10] << 8 | data[11]);
+    printf("Source address: %x, target address: %x\n", source, target);
+
+    puts("UDS data:");
+    for(i = 12; i < dlen; i++)
+    {
+        printf("0x%x ", data[i]);
+    }
+    printf("\n");
+
+
+
+    return 0;
+
+
+}
+
+
 
 //int doip_data_request(doip_sa sa, doip_ta ta, doip_tat tat , uint8_t* data, uint32_t dlen)
 int doip_send_udp(doip_sa sa, doip_ta ta, uint16_t payload_type, uint8_t* data, uint32_t dlen)
@@ -27,6 +88,8 @@ int doip_send_udp(doip_sa sa, doip_ta ta, uint16_t payload_type, uint8_t* data, 
     local.port = 41123;
 
     uint32_t payload_len = dlen + 4;    //TA and SA should also be included in the payload length!
+    //Not sure if it's always true though... What if TA and SA are 0 (e.g. with Veh. Id. Req)? Then those aren't added to payload, thus, no +4?
+
     int msglen = 0;     //Total length of message to be sent over UDP
 
     uint8_t buf[50] = {'\0'};
@@ -81,8 +144,8 @@ int doip_send_udp(doip_sa sa, doip_ta ta, uint16_t payload_type, uint8_t* data, 
         return -1;
     }
 
-    if((ret = sock_udp_recv(&sock, buf, sizeof(buf), 1 * US_PER_SEC, NULL)) < 0) {
-        if (ret == -ETIMEDOUT) {
+    if((ret = sock_udp_recv(&sock, buf, sizeof(buf), udp_recv_timeout, NULL)) < 0) {
+        if (ret == -ETIMEDOUT) {    //TODO: I think there's still something off with the timeout. It keeps blocking...
             puts("Timed out");
         } else {
             puts("Error receiving message");
@@ -94,8 +157,10 @@ int doip_send_udp(doip_sa sa, doip_ta ta, uint16_t payload_type, uint8_t* data, 
             printf("0x%x ", buf[i]);
         }
         printf("\"\n");
+        doip_print_msg_parsed(buf, ret);
     }
 
+    puts("======================");
     sock_udp_close(&sock);
     xtimer_sleep(1);
 
