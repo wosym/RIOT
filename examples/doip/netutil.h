b1d25894d698bb515cec82dc6fef52ea47d16eb0
Wouter <wsymons@nalys-group.com>
2019-12-04 14:42:43 +0100

    Add comments to doip example

--- examples/doip/netutil.h
+++ examples/doip/netutil.h
@@ -4,7 +4,16 @@
 #include "lwip/netif/netdev.h"
 #include "stm32_eth.h"
 
+/*
+ * @brief Initialise network interface and dhcp
+ *
+ * @detailed This used to be in the lwip-bootstrap sequence, but was moved to a seperate function to be called by user, because it didn't work where it originally was.
+ */
 int start_dhcp(void);
+
+/*
+ * @brief shows network interfaces with their MAC adresses and IP's
+ */
 int ifconfig(int argc, char **argv);
 
 
