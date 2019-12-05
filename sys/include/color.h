a18af06d0386f0b4f9a68b947f6b6eba2fc6dfa3
Wouter <wsymons@nalys-group.com>
2019-12-05 12:52:26 +0100

    added color.h for doip

--- sys/include/color.h
+++ sys/include/color.h
@@ -1,148 +1,73 @@
-/*
- * Copyright (C) 2014 - 2016 Freie Universität Berlin
- *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
- */
+//Regular text
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
 
-/**
- * @defgroup    sys_color Color
- * @ingroup     sys
- * @brief       The color sys module supports handling RGB and HSV color
- * @{
- *
- * @file
- * @brief       Headers for the color handling module
- *
- * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
- * @author      Cenk Gündoğan <mail@cgundogan.de>
- * @author      Simon Brummer <brummer.simon@googlemail.com>
- */
+//Regular bold text
+#define BBLK "\e[1;30m"
+#define BRED "\e[1;31m"
+#define BGRN "\e[1;32m"
+#define BYEL "\e[1;33m"
+#define BBLU "\e[1;34m"
+#define BMAG "\e[1;35m"
+#define BCYN "\e[1;36m"
+#define BWHT "\e[1;37m"
 
-#ifndef COLOR_H
-#define COLOR_H
+//Regular underline text
+#define UBLK "\e[4;30m"
+#define URED "\e[4;31m"
+#define UGRN "\e[4;32m"
+#define UYEL "\e[4;33m"
+#define UBLU "\e[4;34m"
+#define UMAG "\e[4;35m"
+#define UCYN "\e[4;36m"
+#define UWHT "\e[4;37m"
 
-#include <stdint.h>
+//Regular background
+#define BLKB "\e[40m"
+#define REDB "\e[41m"
+#define GRNB "\e[42m"
+#define YELB "\e[43m"
+#define BLUB "\e[44m"
+#define MAGB "\e[45m"
+#define CYNB "\e[46m"
+#define WHTB "\e[47m"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+//High intensty background
+#define BLKHB "\e[0;100m"
+#define REDHB "\e[0;101m"
+#define GRNHB "\e[0;102m"
+#define YELHB "\e[0;103m"
+#define BLUHB "\e[0;104m"
+#define MAGHB "\e[0;105m"
+#define CYNHB "\e[0;106m"
+#define WHTHB "\e[0;107m"
 
-/**
- * @brief Data-structure describing a RGB color
- */
-typedef struct {
-    uint8_t r;          /**< red value      [0 - 255] */
-    uint8_t g;          /**< green value    [0 - 255] */
-    uint8_t b;          /**< blue value     [0 - 255] */
-} color_rgb_t;
+//High intensty text
+#define HBLK "\e[0;90m"
+#define HRED "\e[0;91m"
+#define HGRN "\e[0;92m"
+#define HYEL "\e[0;93m"
+#define HBLU "\e[0;94m"
+#define HMAG "\e[0;95m"
+#define HCYN "\e[0;96m"
+#define HWHT "\e[0;97m"
 
-/**
- * @brief RGBA color value
- */
-typedef struct {
-    color_rgb_t color;  /**< RGB value */
-    uint8_t alpha;      /**< alpha value [0 - 255] */
-} color_rgba_t;
+//Bold high intensity text
+#define BHBLK "\e[1;90m"
+#define BHRED "\e[1;91m"
+#define BHGRN "\e[1;92m"
+#define BHYEL "\e[1;93m"
+#define BHBLU "\e[1;94m"
+#define BHMAG "\e[1;95m"
+#define BHCYN "\e[1;96m"
+#define BHWHT "\e[1;97m"
 
-/**
- * @brief Data-structure for holding HSV colors
- */
-typedef struct {
-    float h;            /**< hue value        [0.0 - 360.0] */
-    float s;            /**< saturation value [0.0 - 1.0] */
-    float v;            /**< value            [0.0 - 1.0] */
-} color_hsv_t;
+//Reset
+#define reset "\e[0m"
 
-/**
- * @brief Convert RGB color to HSV color
- *
- * @param[in] rgb       Input color encoded in RGB space
- * @param[out] hsv      Output color encoded in HSV space
- */
-void color_rgb2hsv(color_rgb_t *rgb, color_hsv_t *hsv);
-
-/**
- * @brief Convert HSV color to RGB color
- *
- * @param[in] hsv       Input color encoded in HSV space
- * @param[out] rgb      Output color encoded in RGB space
- */
-void color_hsv2rgb(color_hsv_t *hsv, color_rgb_t *rgb);
-
-/**
- * @brief Convert a @p hex value of the form 0x00RRGGBB to an RGB color struct
- *
- * @note                the two most significant bytes of @p hex will be ignored
- *
- * @param[in] hex       Input color encoded in hex
- * @param[out] rgb      Output color encoded in RGB space
- */
-void color_hex2rgb(const uint32_t hex, color_rgb_t *rgb);
-
-/**
- * @brief Convert a @p rgb struct to a @p hex value of the form 0x00RRGGBB
- *
- * @note                the two most significant bytes of @p hex will be 0
- *
- * @param[in] rgb       Input color encoded in RGB space
- * @param[out] hex      Output color encoded in hex
- */
-void color_rgb2hex(const color_rgb_t *rgb, uint32_t *hex);
-
-/**
- * @brief Convert a hex color string of the form 'RRGGBB' to a color_rgb_t struct
- *
- * @note                @p str MUST contain only hexadecimal digits.
- *                      Expect unexpected behaviour, otherwise.
- *
- * @param[in] str       Input color encoded as string of the form 'RRGGBB'
- * @param[out] color    Output color encoded in RGB space
- */
-void color_str2rgb(const char *str, color_rgb_t *color);
-
-/**
- * @brief Convert a color_rgb_t struct to a hex color string of the form 'RRGGBB'
- *
- * @note                @p str MUST be big enough to hold 6 characters
- *
- * @param[in] rgb       Input color encoded in RGB space
- * @param[out] str      Output color encoded as string of the form 'RRGGBB'
- */
-void color_rgb2str(const color_rgb_t *rgb, char *str);
-
-/**
- * @brief Invert a given rgb color
- *
- * @pre                 ((rgb != NULL) && (inv_rgb != NULL))
- *
- * @param[in] rgb       Input rgb color, that should be converted. Must be NOT NULL
- * @param[out] inv_rgb  Output rgb color, result of the conversion. Must be NOT NULL
- */
-static inline void color_rgb_invert(const color_rgb_t *rgb, color_rgb_t *inv_rgb)
-{
-    inv_rgb->r = rgb->r ^ 0xFF;
-    inv_rgb->g = rgb->g ^ 0xFF;
-    inv_rgb->b = rgb->b ^ 0xFF;
-}
-
-/**
- * @brief Calculate the complementary color of a given rgb color.
- *
- * @note                Complementary color calculation according to adobe illustator calculations.
- *                      See https://helpx.adobe.com/illustrator/using/adjusting-colors.html
- *
- * @pre                 ((rgb != NULL) && (comp_rgb != NULL))
- *
- * @param[in] rgb       Input rgb color. Must be NOT NULL
- * @param[out] comp_rgb Output rgb color, result of the complementary color calculation. Must be NOT NULL
- */
-void color_rgb_complementary(const color_rgb_t *rgb, color_rgb_t *comp_rgb);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* COLOR_H */
-/** @} */
