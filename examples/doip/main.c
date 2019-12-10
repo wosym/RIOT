a9c41144275a86b4a519e4e19e4258345e97cd37
Wouter Symons <wsymons@nalys-group.com>
2019-12-10 15:03:23 +0100

    doip: main make IP selectable from shell

--- examples/doip/main.c
+++ examples/doip/main.c
@@ -1,17 +1,77 @@
-#include <stdio.h>
-#include <xtimer.h>
+#include <ctype.h>
 #include <doip.h>
 #include <shell.h>
+#include <stdio.h>
+#include <string.h>
+#include <xtimer.h>
+
 #include "netutil.h"
 
 #define T_2_SEC 2
 #define T_1_SEC 1
 #define SAMPLE_SA 0xe801
 #define SAMPLE_TA 0x1fff
-#define SAMPLE_IP "192.168.0.11";
+#define SAMPLE_IP "192.168.0.11"
+#define IPSTRLEN 16
 
 sock_doip_t sock;
+char target_ip[IPSTRLEN] = SAMPLE_IP;
 
+static int print_target_ip(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+
+    printf("Current target IP: %s\n", target_ip);
+
+    return 0;
+}
+static int set_target_ip(int argc, char **argv)
+{
+    int i = 0;
+    uint8_t ip_len = 0; //len of ipstring
+    uint8_t nd = 0;     //number of decimal points
+    uint8_t pd = 1;     //previous character was a decimal point flag
+
+    if(argc < 2) {
+        puts("Usage: set_target_ip <ip>");
+        return -1;
+    }
+    ip_len = strlen(argv[1]);
+    if( ip_len > IPSTRLEN)
+    {
+        puts("Wrong IP format. Required format (a): xxx.xxx.xxx.xxx");
+        return -1;
+    }
+
+    //Check if ip is of correct format  //TODO: seperate util-function?
+    for(i = 0; i < ip_len; i++) {
+        if(argv[1][i] == '.') {    //char is a .
+            if(pd) {            //previous char was also a . --> not allowed
+                puts("Wrong IP format (b). Required format: xxx.xxx.xxx.xxx");
+                return -1;
+            } else {            //previous char was not a .
+                nd++;
+                pd=1;
+            }
+        } else {                //char is not a .
+            if(!isdigit((unsigned char)argv[1][i])) {
+                puts("Wrong IP format (c). Required format: xxx.xxx.xxx.xxx");
+                return -1;
+            }
+            pd = 0;
+        }
+    }
+    if(pd) {                    //last char was a . --> not allowed
+        puts("Wrong IP format (d). Required format: xxx.xxx.xxx.xxx");
+        return -1;
+    }
+
+    strncpy(target_ip, argv[1], IPSTRLEN);
+    printf("New target IP: %s\n", target_ip);
+
+    return 0;
+}
 static int spam_doip(int argc, char **argv)
 {
     (void)argc;
@@ -70,6 +130,8 @@ help:
 
 const shell_command_t shell_commands[] = {
     { "ifconfig", "Show network interfaces", ifconfig },
+    { "set_target_ip", "Set IP address to which the DoIP messages should be sent", set_target_ip },
+    { "print_target_ip", "Print current IP address to which the DoIP messages should be sent", print_target_ip },
     { "spam_doip", "Continiously send doip requests", spam_doip },
     { "send_doip", "Send doip message", send_doip },
     { NULL, NULL, NULL }
