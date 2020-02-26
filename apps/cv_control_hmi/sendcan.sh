dad540e23a2c009e2379aad8496a851f1c474633
Wouter Symons <wsymons@nalys-group.com>
2020-02-26 17:46:05 +0100

    drive display over can

---
+++ apps/cv_control_hmi/sendcan.sh
@@ -0,0 +1,12 @@
+echo "Sending stuff over CAN"
+cansend can0 001#48656c6c6f
+sleep 1
+cansend can0 001#52494f5421
+sleep 1
+cansend can0 001#4c4344
+sleep 1
+cansend can0 001#6f766572
+sleep 1
+cansend can0 001#43414e21
+sleep 1
+
